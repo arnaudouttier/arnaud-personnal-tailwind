8fc2abec0b062b1adf34d4c637774b2933f1d047
Arnaud <arnoutti@gmail.com>
2020-07-28 16:25:36 +0200

    master branch cht overlay.js

--- public/js/nav_overlay.js
+++ public/js/nav_overlay.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var menu  = document.getElementsByClassName("nav-toggle")[0];
 menu.addEventListener("click", navBurger);
 
 function navBurger() {
   document.getElementById("inner").classList.toggle("open");
-=======
-
-document.getElementsByClassName("nav-toggle").addEventListener("click", navBurger);
-
-function navBurger() {
-  document.getElementsByClassName("inner").classList.toggle("open");
->>>>>>> a8563047e1a1404cc5a27758951f320a345c41f2
-=======
-document.getElementsByClassName("nav-toggle").addEventListener("click", navBurger);
-
-function navBurger() {
-  document.getElementsByClassName("inner").classList.toggle("open");
->>>>>>> 3570887b0ef73a458d40f2e60928e1b639ce6219
 }

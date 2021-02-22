e0188a43d3bbb55b331b6b68976d137cf79768c8
ao <arnaudouttier@github.com>
2021-02-22 12:31:11 +0100

    nouveau site 0.1

--- public/js/nav_overlay.js
+++
@@ -1,6 +0,0 @@
-var menu  = document.getElementsByClassName("nav-toggle")[0];
-menu.addEventListener("click", navBurger);
-
-function navBurger() {
-  document.getElementById("inner").classList.toggle("open");
-}

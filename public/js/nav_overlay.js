2e1c6fd4d645dfbacb94ce7a97e3c3696f64823d
ao <arnaudouttier@github.com>
2021-02-22 12:52:08 +0100

    gh-pages nouveau site 0.

--- public/js/nav_overlay.js
+++
@@ -1,9 +0,0 @@
-var navToggle = document.querySelectorAll(".nav-toggle");
-var i;
-for (i = 0; i < navToggle.length; i++) {
-  navToggle[i].addEventListener("click", navBurger);
-} 
-
-function navBurger() {
-  document.getElementById("inner").classList.toggle("open");
-}

9c1214be2bbdac069bfb1db63d198e3cb9977405
arnaud.outtier <47449100+arnaudouttier@users.noreply.github.com>
2021-04-06 14:04:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var app = new Vue({
-    el: '#app',
+    el: '#app ',
     data: {
-        email: ' ',
+        sent: false,
+        form: {
+            emailName: ""
+        },
         valid: ' ',
         formMessage: '',
         validInput: '',
@@ -11,13 +14,24 @@ var app = new Vue({
     },
     methods: {
         validEmail() {
-            if (!/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(this.email)) {
+            if (!/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(this.form.emailName)) {
                 this.valid = false;
                 return this.formMessage = ' Please provide a valid email '
             } else {
                 this.valid = true;
                 this.formMessage = ' Email added '
             }
+        },
+        onSubmit(e) {
+            e.preventDefault();
+            this.$axios
+                .post(
+                    "https://arnaudouttier.github.io/ao/mail.php",
+                    querystring.stringify(this.form)
+                )
+                .then(res => {
+                    this.sent = true;
+                });
         }
     }
 })

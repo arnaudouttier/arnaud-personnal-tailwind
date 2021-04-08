6437cb46f1991beec6e7dbf461c37da5b01f1ec9
arnaud.outtier <47449100+arnaudouttier@users.noreply.github.com>
2021-04-08 09:52:40 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ var app = new Vue({
     el: '#app ',
     data: {
         errors: [],
-        email: null,
         valid: true,
         validInput: 'validInput',
         errorInput: 'errorInput',
+        email: ''
     },
     methods: {
         checkForm(e) {
             this.errors = [];
 
-            if (!this.email ||!this.validEmail(this.email)) {
-                this.errors.push('Email valide requis.');
+            if (!this.email || !this.validEmail(this.email)) {
+                this.errors.push('Email requis.');
                 this.valid = false;
-            } 
-            else {
+            } else {
                 this.errors.push('Email ajouté');
                 this.valid = true;
             }
@@ -25,10 +24,25 @@ var app = new Vue({
             }
 
             e.preventDefault();
+
         },
         validEmail(email) {
             var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
+        },
+        sendEmail(e) {
+            try {
+                emailjs.sendForm('Yservice_yk00zem', 'template_i8imi9p', e.target,
+                    'user_9QPd6cAymGUHuxXUoaA86', {
+                        email: this.email
+                    })
+
+            } catch (error) {
+                console.log({ error })
+            }
+            // Reset form field
+            this.email = ''
         }
+
     }
 })
